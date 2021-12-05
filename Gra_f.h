b641bf55e016ad6b9b602b8a30053e825d6715ca
ZuzannaKozlowska182270 <93452233+ZuzannaKozlowska182270@users.noreply.github.com>
2021-12-05 22:49:56 +0100

    Add files via upload

---
+++ Gra_f.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+void Gra::zmienne()
+{
+	this->window = nullptr;
+
+
+	int punkty;
+	float spawn_przeciwnicy;
+	float spawn_przeciwnicy;
+	int max_przeciwnicy;
+	this->punkty = 0;
+	this->timer_przeciwnicy = 0.f;
+	this->spawnTimerMax_przeciwnicy = 1000.f;
+	int max_przeciwnicy = 3;
+}
